60d0dbabd37d7008a296bfe4e9a276f9fe7f01ea
Josue Feitosa <josue.feitosa@outlook.com.br>
2020-08-07 18:10:35 -0300

    ESLINT, Prettier, Typescript and express setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,14 @@
+import dotenv from 'dotenv';
+
 import express from 'express';
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3333);
+const host = process.env.SERVER_HOST;
+const port = parseInt(process.env.SERVER_PORT);
+
+app.listen(port, host, () => {
+  console.log(`🚀Server running at http://${host}:${port}`);
+});
